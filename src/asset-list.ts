dd46ed82f111b7ab81799e544c064de2a9165505
dkent600 <github@douglaskent.com>
2025-07-05 16:07:50 -0600

    fixed lots of compile errors with vite and typescript configuration

--- src/asset-list.ts
+++ src/asset-list.ts
@@ -1,16 +1,49 @@
 import { customElement } from 'aurelia';
 
+interface IAsset {
+  name: string;
+  exchange: string;
+  exchangeName: string;
+  percentage: number;
+  apiUrl: string;
+}
+
 @customElement('asset-list')
 export class AssetList {
-  assets = [];
+  assets: Array<IAsset> = [];
 
   async binding() {
+    console.log("asdasd");
     // Fetch config.json from the public root
     const response = await fetch('/config.json');
     const config = await response.json();
     const exchanges = (config.batchConfig?.exchanges ?? []);
-    const exchangeMap = Object.fromEntries(exchanges.map(e => [e.name, e]));
-    this.assets = (config.batchConfig?.assets ?? []).map(asset => ({
+    const exchangeMap = Object.fromEntries(exchanges.map((e: { name: string; }) => [e.name, e]));
+    interface IExchange {
+      name: string;
+      apiUrl?: string;
+      [key: string]: unknown;
+    }
+
+    interface IAssetConfig {
+      name: string;
+      exchange: string;
+      percentage?: number;
+      [key: string]: unknown;
+    }
+
+    interface IBatchConfig {
+      exchanges?: IExchange[];
+      assets?: IAssetConfig[];
+      [key: string]: unknown;
+    }
+
+    interface IConfig {
+      batchConfig?: IBatchConfig;
+      [key: string]: unknown;
+    }
+
+    this.assets = ((config as IConfig).batchConfig?.assets ?? []).map((asset: IAssetConfig): IAsset => ({
       name: asset.name,
       exchange: asset.exchange,
       exchangeName: asset.exchange,
