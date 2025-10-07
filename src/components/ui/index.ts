8886fef3f571d0d76f4a2bf9ff0005a8cc272940
dkent600 <github@douglaskent.com>
2025-10-06 18:33:27 -0600

    backing out data-grid

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,4 @@
 // UI Components Index
-export { DataGrid } from './data-grid/data-grid.js';
 export { TableComponent } from './table-component/table-component.js';
 export { TableComponentRow } from './table-component/table-component-row.js';
 export { TableComponentCell } from './table-component/table-component-cell.js';
