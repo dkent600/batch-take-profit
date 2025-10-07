c1668ee1badd4c8edd6e31cdf410f2400a67f36a
dkent600 <github@douglaskent.com>
2025-10-06 18:49:17 -0600

    removed index.ts

--- src/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-// UI Components Index
-export { TableComponent } from './table-component/table-component.js';
-export { TableComponentRow } from './table-component/table-component-row.js';
-export { TableComponentCell } from './table-component/table-component-cell.js';
